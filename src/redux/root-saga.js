5f092310eb59edee78b0c610386e06b014b065b1
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-07-12 11:19:34 +0300

    Added root-reducer.js, root-saga.js, and store.js. In addition, added package react-widgets for multi-select purposes.

---
+++ src/redux/root-saga.js
@@ -0,0 +1,9 @@
+import { all, call } from 'redux-saga/effects'
+
+import { ingredientsSagas } from './ingredients/ingredients.sagas'
+
+export default function* rootSaga() {
+  yield all([
+    call(ingredientsSagas),
+  ])
+}
