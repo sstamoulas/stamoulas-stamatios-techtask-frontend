6efee2307709d03d8e2b1042e85ceccbd335c4e5
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-07-14 03:57:39 +0300

    Added UPDATE_MEAL_DATE for mealDate reducer and test

--- src/redux/mealDate/mealDate.reducer.js
+++ src/redux/mealDate/mealDate.reducer.js
@@ -1,10 +1,16 @@
 import MealDateActionTypes from './mealDate.types'
 
 const INITIAL_STATE = {
+  mealDate: new Date(),
 }
 
 const mealDateReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case MealDateActionTypes.UPDATE_MEAL_DATE:
+      return {
+        ...state,
+        mealDate: action.payload,
+      }
     default:
       return state
   }
--- src/redux/mealDate/mealDate.reducer.test.js
+++ src/redux/mealDate/mealDate.reducer.test.js
@@ -2,10 +2,24 @@ import MealDateActionTypes from './mealDate.types'
 import mealDateReducer from './mealDate.reducer'
 
 const initialState = {
+  mealDate: new Date(),
 }
 
 describe('mealDateReducer', () => {
   it('should return initial state', () => {
     expect(mealDateReducer(undefined, {})).toEqual(initialState)
+  })
+
+  it('should set mealDate to payload if updateMealDate action', () => {
+    const mockItems = new Date()
+    expect(
+      mealDateReducer(initialState, {
+        type: MealDateActionTypes.UPDATE_MEAL_DATE,
+        payload: mockItems
+      })
+    ).toEqual({
+      ...initialState,
+      mealDate: mockItems
+    })
   })
 })
