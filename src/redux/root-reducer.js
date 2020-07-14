a3fc7366c180217f43c8c5b0f235d8404a543ed3
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-07-14 04:01:05 +0300

    Added mealDate to root-reduer.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 
 import ingredientsReducer from './ingredients/ingredients.reducer'
+import mealDateReducer from './mealDate/mealDate.reducer'
 import recipesReducer from './recipes/recipes.reducer'
 
 const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
+  mealDate: mealDateReducer,
   recipes: recipesReducer,
 })
 
