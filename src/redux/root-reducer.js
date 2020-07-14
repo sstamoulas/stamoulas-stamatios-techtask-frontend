28e81d9127eca1c8f5a113df50b4587c5ff82428
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-07-14 03:39:17 +0300

    Added recipes to root-reducer.js and root-saga.js

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import ingredientsReducer from './ingredients/ingredients.reducer'
+import recipesReducer from './recipes/recipes.reducer'
 
 const rootReducer = combineReducers({
   ingredients: ingredientsReducer,
+  recipes: recipesReducer,
 })
 
 export default rootReducer
--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,9 +1,11 @@
 import { all, call } from 'redux-saga/effects'
 
 import ingredientsSagas from './ingredients/ingredients.sagas'
+import recipesSagas from './recipes/recipes.sagas'
 
 export default function* rootSaga() {
   yield all([
     call(ingredientsSagas),
+    call(recipesSagas),
   ])
 }
