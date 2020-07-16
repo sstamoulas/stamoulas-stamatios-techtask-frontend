06cf9cc372df4e05658209893aefd74e74ad9db5
Stamatios Stamoulas <stamatios.stamoulas@gmail.com>
2020-07-16 11:31:20 +0300

    Updates

---
+++ precache-manifest.5084ff22350a67ed7b77e1a27e18176d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7fe7085d96330a5d349c2daff86fb1",
+    "url": "/stamoulas-stamatios-techtask-frontend/index.html"
+  },
+  {
+    "revision": "118d322ed6faae84f3e3",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/css/2.a575a431.chunk.css"
+  },
+  {
+    "revision": "e0232b445fe96890d0f4",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/css/main.7fcf649a.chunk.css"
+  },
+  {
+    "revision": "118d322ed6faae84f3e3",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/js/2.e33d7321.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/js/2.e33d7321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0232b445fe96890d0f4",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/js/main.a04216e5.chunk.js"
+  },
+  {
+    "revision": "f33244bc54881630fb77",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/js/runtime-main.4c455198.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/stamoulas-stamatios-techtask-frontend/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
